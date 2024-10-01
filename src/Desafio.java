ccb92f9a73232f814ebe410998a4441b2def5a55
manuel <manuelfelipe1713@gmail.com>
2024-10-01 16:07:34 -0500

    Adicionar datos del cliente

---
+++ src/Desafio.java
@@ -0,0 +1,14 @@
+public class Desafio {
+
+    public static void main(String[] args) {
+
+        String nombre = "Tony Stark";
+        String tipoDeCuenta = "Corriente";
+        double saldo = 1500000;
+
+        System.out.println("************");
+        System.out.println("\nNombre del cliente: " + nombre);
+        System.out.println("El tipo de cuenta es: " + tipoDeCuenta);
+        System.out.println("Su saldo disponible es: " + saldo);
+    }
+}
